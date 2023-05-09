b933e64446670d1292ce1d2d401e3919697dbfd2
Ivan Klimov <klimov128@gmail.com>
2023-05-09 16:35:29 +0300

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -21,7 +21,7 @@ export class Searchbar extends Component {
       <header className="searchbar">
         <form className="form" onSubmit={this.handleSubmit}>
           <button type="submit" className="button">
-            <span className="button-label">Search</span>
+            <span className="button-label">Search_</span>
           </button>
 
           <input
