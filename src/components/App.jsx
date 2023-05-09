770a99f1ab46cddffb9aab3ce4f319a55bb2fa17
Ivan Klimov <klimov128@gmail.com>
2023-05-09 14:36:08 +0300

    add react components
    seachbar
    image gallery
    image gallery item

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+
+export class App extends Component {
+  state = { searchValue: '', images: [] };
+
+  componentDidUpdate(nextProps, nextState) {
+    console.log('this.state.searchValue', this.state.searchValue);
+    console.log('nextState', nextState.searchValue);
+    if (this.state.searchValue !== nextState.searchValue) {
+      fetch(
+        `https://pixabay.com/api/?q=${this.state.searchValue}&page=1&key=34585976-51a68d3a5f9444fd8119e93c8&image_type=photo&orientation=horizontal&per_page=12`
+      )
+        .then(res => res.json())
+        .then(data => {
+          // console.log(data.hits);
+          return this.setState({ images: data.hits });
+        });
+    }
+  }
+
+  seachQery = qery => {
+    this.setState({ searchValue: qery });
+  };
+
+  render() {
+    return (
+      <>
+        <Searchbar onSubmitProp={this.seachQery} />
+        <ImageGallery>
+          <ImageGalleryItem images={this.state.images} />
+        </ImageGallery>
+      </>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+
+export class Searchbar extends Component {
+  state = {
+    input: '',
+  };
+
+  handleChange = e => {
+    this.setState({ [e.currentTarget.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmitProp(this.state.input);
+    console.log(this.state.input);
+    this.setState({ input: '' });
+  };
+
+  render() {
+    return (
+      <header className="searchbar">
+        <form className="form" onSubmit={this.handleSubmit}>
+          <button type="submit" className="button">
+            <span className="button-label">Search</span>
+          </button>
+
+          <input
+            className="input"
+            type="text"
+            name="input"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            onChange={this.handleChange}
+            value={this.state.input}
+          />
+        </form>
+      </header>
+    );
+  }
+}
