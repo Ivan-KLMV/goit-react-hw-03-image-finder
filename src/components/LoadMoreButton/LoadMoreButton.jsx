f61491e8f8cec408ce70dbea059d243852adbc0c
Ivan Klimov <klimov128@gmail.com>
2023-05-11 21:02:09 +0300

    add styles

--- src/components/LoadMoreButton/LoadMoreButton.jsx
+++ src/components/LoadMoreButton/LoadMoreButton.jsx
@@ -1,7 +1,9 @@
+import { LoadMoreButtonStyled } from './LoadMoreButton.styled';
+
 export const LoadMoreButton = ({ loadMoreHandle }) => {
   return (
-    <button type="LoadMoreButton" onClick={loadMoreHandle}>
+    <LoadMoreButtonStyled type="button" onClick={loadMoreHandle}>
       Load more
-    </button>
+    </LoadMoreButtonStyled>
   );
 };
